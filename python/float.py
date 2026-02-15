f407a21220fcfe99219343dec2d72327446fabf3
Manasi Dhumal <Manasidhumal1999@gmail.com>
2026-02-15 14:37:09 +0530

    adding float file

---
+++ python/float.py
@@ -0,0 +1,13 @@
+a=2.0
+b=2.5
+addition=a+b
+print ("Addition:",addition)
+subtraction=a-b
+print ("Subtraction:", subtraction)
+multiplication=a*b
+print("Multiplication:" ,multiplication)
+division=a/b
+print("Division:", division)
+result=round(3.14159265359,2)
+result=round(3.14159265359,3)  #3.141
+print("Round:" ,result)
