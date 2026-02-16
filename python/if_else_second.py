57e307c77ae1174b4c466310227d7c2e2363a7eb
Manasi Dhumal <Manasidhumal1999@gmail.com>
2026-02-16 13:00:06 +0530

    if_else_secnf programm authentications

---
+++ python/if_else_second.py
@@ -0,0 +1,8 @@
+username = "admin"
+#password = "admin123"
+password = "admin000"
+
+if username == "admin" and password == "admin123":
+    print("Login successful")
+else:
+    print("Invalid credentials")
